ef56a026816c6cb56c33f4c9e9033ed47001750d
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-04 02:55:50 +0900

    변수 설정

---
+++ This_is_Csharp_00/This_is_CSharp_01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace This_is_CSharp_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 실수형 데이터
+            float f = 5.2f;
+            double d = 10.5;
+            decimal money = 200.099m;
+
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(money);
+        }
+    }
+}
---
+++ This_is_Csharp_00/This_is_CSharp_02/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace This_is_CSharp_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch1 = '\t';
+            char ch2 = 'T';
+            char ch3 = '\n';
+            char ch4 = 'o';
+
+            Console.WriteLine(ch1);
+            Console.WriteLine(ch2);
+            Console.WriteLine(ch3);
+            Console.WriteLine(ch4);
+
+            char ch = '\u25b6';
+            Console.WriteLine(ch);
+
+            string text = "Hello World!";
+            Console.WriteLine(text);
+        }
+    }
+}
