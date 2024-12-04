5cabede86cd09353019e3b500bdba37048c53222
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-05 03:19:45 +0900

    캐스팅

---
+++ This_is_Csharp_00/Tihis_is_CSharp_03/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Tihis_is_CSharp_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort u = 65;
+            char c = (char)u;
+
+            Console.WriteLine(c);
+            Console.WriteLine(u);
+        }
+    }
+}
