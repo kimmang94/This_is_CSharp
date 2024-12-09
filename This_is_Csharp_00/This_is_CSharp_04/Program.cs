a16f72bd9c7003db919255311e159713c089eada
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-09 17:36:01 +0900

    이것이 C#이다

---
+++ This_is_Csharp_00/This_is_CSharp_04/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace This_is_CSharp_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 변수 선언 방법
+            int x;
+            x = 100;
+
+            int y = 100;
+
+            int a, b, c;
+            int z = 30, k = 20, l = 50;
+        }
+    }
+}
---
+++ This_is_Csharp_00/This_is_CSharp_05/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace This_is_CSharp_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
