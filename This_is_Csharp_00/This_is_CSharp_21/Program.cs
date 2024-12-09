34e574bec289ff9614eb4de22f629565bed29d82
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-09 19:15:07 +0900

    이것이 C#이다

---
+++ This_is_Csharp_00/This_is_CSharp_21/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace This_is_CSharp_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_INT = 2147483647;
+            const int MIN_INT = -2147483648;
+
+            Console.WriteLine(MAX_INT);
+            Console.WriteLine(MIN_INT);
+        }
+    }
+}
