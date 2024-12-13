0c8b6927cd86590519da8108a74c6675931fdad8
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-14 03:43:31 +0900

    이것이 C# 이다

---
+++ This_is_Csharp_00/This_is_CSharp_22/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace This_is_CSharp_22
+{
+    internal class Program
+    {
+        enum DialogResult
+        {
+            YES,
+            NO,
+            CANCEL,
+            CONFIRM,
+            OK,
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int)DialogResult.YES);
+            Console.WriteLine((int)DialogResult.NO);
+            Console.WriteLine((int)DialogResult.CANCEL);
+            Console.WriteLine((int)DialogResult.CONFIRM);
+            Console.WriteLine((int)DialogResult.OK);
+        }
+    }
+}
