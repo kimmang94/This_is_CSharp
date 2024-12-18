7df2901e2c9bcb392a065d2c99f4aec3d4f7df47
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-19 04:21:44 +0900

    이것이 C#이다

--- This_is_Csharp_00/This_is_Csharp_35/Program.cs
+++ This_is_Csharp_00/This_is_Csharp_35/Program.cs
@@ -4,8 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ex035 ex035 = new Ex035();
+            ex035.Run();
+        }
+
+        public class Ex035
+        {
+            public void Run()
+            {
+                Console.WriteLine("숫자를 입력해주세요");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                bool isOddNumber = ((number % 2 == 1) ? true : false);
+
+                if (number > 0 && isOddNumber)
+                {
+                    Console.WriteLine("{0}은 양의 홀수 입니다.", number);
+                }
+                else if (number > 0 && !isOddNumber)
+                {
+                    Console.WriteLine("{0}은 짝수입니다.", number);
+                }
+                else if (number < 0 && isOddNumber)
+                {
+                    Console.WriteLine("{0}은 음의 홀수입니다.", number);
+                }
+                else if (number < 0 && !isOddNumber)
+                {
+                    Console.WriteLine("{0}은 음의 짝수입니다.", number);
+                }
+                else
+                {
+                    Console.WriteLine("{0}은 zero 입니다", number);
+                }
+            }
         }
     }
 }
-
