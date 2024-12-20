52f0b6f277870923bde63d151e3ca8455474beca
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-20 10:55:21 +0900

    이것이 C#이다

--- This_is_Csharp_00/This_is_Csharp_50/Program.cs
+++ This_is_Csharp_00/This_is_Csharp_50/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ex050 ex050 = new Ex050();
+            ex050.Run();
+        }
+        public class Ex050
+        {
+            public void Run()
+            {
+                Bread custard_cream_bread = new Bread();
+                custard_cream_bread.Shape = "별모양";
+                custard_cream_bread.Source = "슈크림";
+
+                Console.WriteLine("{0} {1} 빵", custard_cream_bread.Shape, custard_cream_bread.Source);
+
+                Bread pizza_bread = new Bread();
+                pizza_bread.Shape = "네모난";
+                pizza_bread.Source = "피자";
+                Console.WriteLine("{0} {1} 빵", pizza_bread.Shape, pizza_bread.Source);
+            }
+
+            class Bread
+            {
+                public string Shape { get; set; }
+                public string Source { get; set; }
+            }
         }
     }
 }
