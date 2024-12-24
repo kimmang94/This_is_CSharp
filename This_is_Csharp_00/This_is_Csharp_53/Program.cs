9fc716feb28277b98a36b0cc1ff908ae0e3aa4c9
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-24 10:32:53 +0900

    이것이 C#이다.

--- This_is_Csharp_00/This_is_Csharp_53/Program.cs
+++ This_is_Csharp_00/This_is_Csharp_53/Program.cs
@@ -10,6 +10,64 @@ namespace This_is_Csharp_53
     {
         static void Main(string[] args)
         {
+            Ex053 ex053 = new Ex053();
+            ex053.Run();
         }
+        public class Ex053
+        {
+            public void Run()
+            {
+                GasolineCar008 gasolineCar = new GasolineCar008();
+                gasolineCar.Color = "검정";
+                gasolineCar.Size = "SUV";
+                EletronicCar008 eletronicCar008 = new EletronicCar008();
+                eletronicCar008.Color = "초록";
+                eletronicCar008.Size = "경차";
+
+                Console.WriteLine("{0}색 {1}가", gasolineCar.Color, gasolineCar.Size);
+                gasolineCar.Go();
+                Console.WriteLine("{0}색 {1}가", eletronicCar008.Color, eletronicCar008.Size);
+                eletronicCar008.Left();
+            }
+        }
+
+        public class Car008
+        {
+            public string Color { get; set; }
+            public string Size { get; set; }
+
+            public void Engine_on()
+            {
+                Console.WriteLine("시동을 켭니다");
+            }
+
+            public void Engine_off()
+            {
+                Console.WriteLine("시동을 끕니다");
+            }
+
+            public void Go()
+            {
+                Console.WriteLine("전진합니다");
+            }
+
+            public void Back()
+            {
+                Console.WriteLine("후진 합니다.");
+            }
+
+            public void Left()
+            {
+                Console.WriteLine("좌회전 합니다.");
+            }
+
+            public void Right()
+            {
+                Console.WriteLine("우회전 합니다.");
+            }
+        }
+
+        public class GasolineCar008 : Car008 { }
+        public class EletronicCar008 : Car008 {}
     }
 }
