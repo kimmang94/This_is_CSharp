42b80bff05d7cdc3ee75ebb70f700c570d1254db
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-24 09:40:43 +0900

    이것이 C#이다.

---
+++ This_is_Csharp_00/This_is_Csharp_51/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This_is_Csharp_51
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exam051 exam051 = new Exam051();
+            exam051.Run();
+        }
+
+        public class Exam051
+        {
+            public void Run()
+            {
+                Sum(1, 1);
+                Sum(2, 2);
+                Sum(3, 3);
+            }
+
+            private void Sum(int number01, int number02)
+            {
+                Console.WriteLine("{0} + {1} = {2}", number01, number02, number01 + number02);
+            }
+        }
+    }
+}
---
+++ This_is_Csharp_00/This_is_Csharp_52/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace This_is_Csharp_52
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex052 ex052 = new Ex052();
+            ex052.Run();
+        }
+
+        public class Ex052
+        {
+            public void Run()
+            {
+                int number01 = 10;
+                int number02 = 3;
+
+                dynamic result01 = Sum(number01, number02);
+                dynamic result02 = Minus(number01, number02);
+                dynamic result03 = Multiple(number01, number02);
+                dynamic result04 = Divede(number01, number02);
+
+                Console.WriteLine("{0}과 {1}의 사칙연산 결과값 : {1}, {2}, {3}, {4}, {5}",
+                    number01,
+                    number02,
+                    result01,
+                    result02,
+                    result03,
+                    result04);
+
+            }
+
+            private int Sum(int number01, int number02)
+            {
+                return number01 + number02;
+            }
+            private int Minus(int number01, int number02)
+            {
+                return number01 - number02;
+            }
+            private int Multiple(int number01, int number02)
+            {
+                return number01 * number02;
+            }
+            private double Divede(int number01, int number02)
+            {
+                return (double)number01 / number02;
+            }
+        }
+    }
+}
