ead8290a6851459e10cd5dc1e299db4f8633d28f
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-27 17:01:14 +0900

    이것이 C#이다

--- This_is_Csharp_00/This_is_CSharp_70/Program.cs
+++ This_is_Csharp_00/This_is_CSharp_70/Program.cs
@@ -10,6 +10,48 @@ namespace This_is_CSharp_70
     {
         static void Main(string[] args)
         {
+            Ex070 ex070 = new Ex070();
+            ex070.Run();
         }
+
+        public class Ex070
+        {
+            public void Run()
+            {
+                Queue<string> queue = new Queue<string>();
+
+                for (int idx = 1; idx < 11; idx++)
+                {
+                    queue.Enqueue(string.Format("{0}번 승객", idx));
+                }
+
+                Console.WriteLine(" * 정류장 승객 현황");
+                foreach (object obj in queue)
+                {
+                    Console.WriteLine(obj.ToString());
+                }
+                Console.WriteLine("=======================");
+                Console.WriteLine("버스가 도착했습니다. (6명 승차가능)");
+
+                for (int i = 0; i < 6; i++)
+                {
+                    queue.Dequeue();
+                }
+
+                Console.WriteLine("버스가 출발했습니다.");
+                Console.WriteLine("=======================");
+
+                Console.WriteLine("새로운 승객이 줄을 섭니다");
+                queue.Enqueue("새로운 승객");
+
+                Console.WriteLine("* 정류장 승객 현황");
+                foreach (object obj in queue)
+                {
+                    Console.WriteLine(obj.ToString());
+                }
+                Console.WriteLine("=======================");
+            }
+        }
+        
     }
 }
--- This_is_Csharp_00/This_is_CSharp_71/Program.cs
+++ This_is_Csharp_00/This_is_CSharp_71/Program.cs
@@ -10,6 +10,37 @@ namespace This_is_CSharp_71
     {
         static void Main(string[] args)
         {
+            Ex071 ex071 = new Ex071();
+            ex071.Run();
+        }
+
+        public class Ex071
+        {
+            public void Run()
+            {
+                Stack<string> stk = new Stack<string>();
+
+                Console.WriteLine("* 시작점");
+                for (int idx = 1; idx < 11; idx++)
+                {
+                    Console.WriteLine("{0}번 선수 도착", idx);
+                    stk.Push(string.Format("{0}번 선수", idx));
+                }
+
+                Console.WriteLine("====");
+                Console.WriteLine("5 ~ 10 등 선수는 탈락입니다");
+                for (int idx = 0; idx < 6; idx++)
+                {
+                    stk.Pop();
+                }
+                Console.WriteLine("====");
+
+                Console.WriteLine("* 올림픽 대표선수 명단");
+                foreach(object obj in stk)
+                {
+                    Console.WriteLine(obj.ToString());
+                }
+            }
         }
     }
 }
