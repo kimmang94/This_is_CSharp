16816db2d8b938d389d9b5fdc8227036ec3d2eb2
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-29 12:13:04 +0900

    이것이 C#이다

--- This_is_Csharp_00/This_is_CSharp_81/Program.cs
+++ This_is_Csharp_00/This_is_CSharp_81/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,24 @@ namespace This_is_CSharp_81
     {
         static void Main(string[] args)
         {
+            Ex081 ex081 = new Ex081();
+            ex081.Run();
+        }
+
+        public class Ex081
+        {
+            private readonly string currentDirectory = Environment.CurrentDirectory;
+            public void Run()
+            {
+                DirectoryInfo directoryInfo = new DirectoryInfo(currentDirectory + @"\data");
+                if (!directoryInfo.Exists)
+                {
+                    directoryInfo.Create();
+                    Console.WriteLine("디렉토리가 생성되었습니다");
+                }
+
+                Console.WriteLine("생성 경로 : {0}", directoryInfo.FullName);
+            }
         }
     }
 }
--- This_is_Csharp_00/This_is_CSharp_82/Program.cs
+++ This_is_Csharp_00/This_is_CSharp_82/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace This_is_CSharp_82
     {
         static void Main(string[] args)
         {
+            Ex082 ex082 = new Ex082();
+            ex082.Run();
+        }
+
+        public class Ex082
+        {
+            private readonly string currentDirectory = Environment.CurrentDirectory;
+            public void Run()
+            {
+
+                using (StreamWriter sw = new StreamWriter(currentDirectory + @"\data\log.txt", true))
+                {
+                    sw.WriteLine("프로그램 실행 시간 : {0}", DateTime.Now);
+                }
+            }
         }
     }
 }
