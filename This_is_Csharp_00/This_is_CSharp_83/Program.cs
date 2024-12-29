34e9b54948670444a6c9fdc8d9dca83d88538c29
KIMHYEOK\borad <kimmang940715@naver.com>
2024-12-29 12:52:33 +0900

    이것이 C#이다

--- This_is_Csharp_00/This_is_CSharp_83/Program.cs
+++ This_is_Csharp_00/This_is_CSharp_83/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Ex083 ex083 = new Ex083();
+            ex083.Run();
+        }
+
+        public class Ex083
+        {
+            private readonly string currentDirectory = Environment.CurrentDirectory;
+            public void Run()
+            {
+                FileInfo fileInfo = new FileInfo(currentDirectory + @"\data\log.txt");
+
+                Console.WriteLine("저장 경로 : {0}", fileInfo.DirectoryName);
+                Console.WriteLine("파일명 : {0}", fileInfo.Name);
+
+                Console.WriteLine("=== 파일 내용 ===");
+                using (StreamReader sr = new StreamReader(fileInfo.FullName))
+                {
+                    string line = string.Empty;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
         }
     }
 }
